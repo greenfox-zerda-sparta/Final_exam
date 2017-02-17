3ab63ea6fbcac5ff5d450828623f57879da9328b
tataiferenc89 <tatai.ferenc89@gmail.com>
2017-02-17 10:16:33 +0100

    random fill function added

--- main.cpp
+++ main.cpp
@@ -2,11 +2,15 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 //Create a function that takes two list of numbers and returns a new list that only consists those
 //numbers that are in the first array but not in the second
+
+void fill_two_vector_with_random_integers(vector<int> &vect);
 
 void fill_two_vector_with_integers(vector<int> &vect_1, vector<int> &vect_2);
 
@@ -19,12 +23,17 @@ vector<int> subtract_list_two_from_list_one(vector<int> l_one, vector<int> l_two
 vector<int> subtract_version_two(vector<int> l_one, vector<int> l_two);
 
 int main(int argc, char* argv[]) {
+  
+  srand(time(0));
+
   vector<int> list_one;
   vector<int> list_two;
   vector<int> subtracted_one;
   vector<int> subtracted_two;
 
-  fill_two_vector_with_integers(list_one, list_two);
+  //fill_two_vector_with_integers(list_one, list_two);
+  fill_two_vector_with_random_integers(list_one);
+  fill_two_vector_with_random_integers(list_two);
 
   print_vector_to_console(list_one);
   
@@ -41,6 +50,12 @@ int main(int argc, char* argv[]) {
   print_vector_to_console(subtracted_two);
 
   return 0;
+}
+
+void fill_two_vector_with_random_integers(vector<int> &vect) {
+  for (int i = 0; i < 20; i++) {
+    vect.push_back(rand() % 20);
+  }
 }
 
 void fill_two_vector_with_integers(vector<int> &vect_1, vector<int> &vect_2) {
@@ -65,6 +80,8 @@ void sort_vector_elements(vector<int> &vect) {
 vector<int> subtract_list_two_from_list_one(vector<int> l_one, vector<int> l_two) {
   vector<int> subtracted_vector;
   bool is_element_in_both_vector = false;
+  sort_vector_elements(l_one);
+  sort_vector_elements(l_two);
   for (int i = 0; i < l_one.size(); i++) {
     for (int j = 0; j < l_two.size(); j++) {
       if (l_one[i] == l_two[j]) {
